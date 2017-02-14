ebac38f1f49eaa2de3faba0a56571b4d5ea5760e
Sebastian Łaska <sebastian.laska1@gmail.com>
2017-02-14 23:55:37 +0100

    Excercise 1

--- selenium-testing/java-example/src/test/java/ru/stqa/training/selenium/FirstTest.java
+++ selenium-testing/java-example/src/test/java/ru/stqa/training/selenium/FirstTest.java
@@ -1,4 +1,5 @@
-package ru.stqa.selenium;
+package ru.stqa.training.selenium;
+
 
 import org.junit.After;
 import org.junit.Before;
@@ -10,29 +11,31 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
-
 public class FirstTest {
 
-    private WebDriverWait wait;
     private WebDriver driver;
+    private WebDriverWait wait;
 
     @Before
-    public void start(){
+    public void start()
+    {
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, 10);
 
     }
 
     @Test
-    public void myFirstTest() {
+    public void testMain()
+    {
         driver.get("http://www.google.com");
         driver.findElement(By.name("q")).sendKeys("webdriver");
         driver.findElement(By.name("btnG")).click();
-        wait.until(titleIs("webdriver"));
+        wait.until(titleIs("webdriver - Szukaj w Google"));
     }
 
     @After
-    public void stop() {
+    public void stop()
+    {
         driver.quit();
         driver=null;
     }
